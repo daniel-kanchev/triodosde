096529d9316f9a95c6cf2259f51a1557926dd8aa
daniel-kanchev <daniel.kanchev@adata.pro>
2021-03-15 14:59:58 +0200

    Finished Script

---
+++ README.md
@@ -0,0 +1,11 @@
+URL: https://diefarbedesgeldes.de/
+
+    Spider name: triodosde
+
+DB Schema:
+- title
+- date
+- link
+- content
+
+Notes:
---
+++ triodosde/settings.py
@@ -0,0 +1,13 @@
+BOT_NAME = 'triodosde'
+SPIDER_MODULES = ['triodosde.spiders']
+NEWSPIDER_MODULE = 'triodosde.spiders'
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
+ITEM_PIPELINES = {
+    'triodosde.pipelines.DatabasePipeline': 300,
+}
+FEED_EXPORT_ENCODING = 'utf-8'
+ROBOTSTXT_OBEY = True
+
+LOG_LEVEL = 'WARNING'
+
+# LOG_LEVEL = 'DEBUG'
